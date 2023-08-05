a598a7787c6ef808d83c19370337f0f1db7ede69
jkapa7 <juandaniel5005@gmail.com>
2023-08-04 19:53:30 -0500

    types of values, number,null,string,object, undefined, array of numbers number[] array of string string[], function declaration, set what is received and return

--- prueba.ts
+++ prueba.ts
@@ -2,6 +2,40 @@
 
 //LO QUE SE LEE ES JAVASCRIPT, DE TYPESCRIPT SE TRADUCE A JAVASCRIPT Y ESE CODIGO ES EL QUE SE LEE
 
-const num1 = 5;
+let num = 5;
+console.log(num);
 
-console.log(num1);
+const num1: number = 1;
+
+const saludo: string = "Hola";
+
+const verdad: boolean = true;
+
+const object: object = {};
+//TANTO EN JS COMO EN TS EL TYPEOF DE UN ARRAY ES OBJECT
+const object2: object = [];
+
+//TYPEOF DE NULL EN JAVASCRIPT ES UN OBJETO.
+//PERO EN TYPESCRIPT NULL ES TIPO NULL
+const valor_null: null = null;
+
+const valor: undefined = undefined;
+
+//SI BIEN EXISTE EL TIPO ANY, CON EL QUE PODEMOS ASIGNAR
+//CUALQUIER TIPO DE DATO, NO LO USAREMOS.
+// const cualquier_cosa: any = "";
+
+//ARRAY DE NUMEROS
+const numeros_pares: number[] = [2, 4, 6, 8];
+
+//ARRAY DE STRINGS
+const palabras: string[] = ["hola", "chao", "bye"];
+
+const numeros: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//DEFINO QUE ES LO QUE VA A RECIBIR LA FUNCION.
+//Y DEFINO QUE ES LO QUE VA A RETORNAR LA FUNCION
+function filtrarPares(numbers: number[]): number[] {
+  return numbers.filter((e) => e % 2 == 0);
+}
+console.log(filtrarPares(numeros));
